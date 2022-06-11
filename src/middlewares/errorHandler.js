464d587f8489639263e766378fd56fd8f55f7582
Alexandre <lutosaalexandre@gmail.com>
2022-06-11 00:01:36 -0300

    fix: small correction to error handler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   if (err) {
-    return res.status(err.status || 500).json(err.message || 'Unexpected error'); 
+    return res.status(err.status || 500).json({ message: err.message || 'Unexpected error' }); 
   }
   next();
 };
