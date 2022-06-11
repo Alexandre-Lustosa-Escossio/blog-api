308c3263e121b622064aed66fa3e0de1b6138f94
Alexandre <lutosaalexandre@gmail.com>
2022-06-10 22:49:54 -0300

    feat: postCategory model implemented

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'BlogPosts',
+        foreignKey: 'id'
+      }
+    },
+    categoryId: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Categories',
+        foreignKey: 'id'
+      }
+    },
+  })
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'BlogPosts',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId'
+    })
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'BlogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId'
+    })
+  }
+};
