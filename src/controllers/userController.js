077af312622a0b4cf342c0e9fc9cf1981761d86e
Alexandre <lutosaalexandre@gmail.com>
2022-06-11 01:00:22 -0300

    feat: userController and userService createUser method implemented

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,16 @@
-const createUser = () => {};
+const userService = require('../services/userService');
+const errorMsgs = require('../helpers/errorMessages.json');
+
+const createUser = async (req, res, next) => {
+  try {
+    const response = await userService.createUser(req.body);
+    return res.status(201).json(response);
+  } catch (e) {
+    e.message = errorMsgs.userAlreadyRegistered;
+    e.status = 409;
+    next(e);
+  }
+};
 
 module.exports = {
   createUser,
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,10 @@
-const createUser = () => {};
+const { User } = require('../database/models');
+
+const createUser = async (body) => {
+    const response = await User.create(body);
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjo1LCJkaXNwbGF5TmFtZSI6InVzdWFyaW8gZGUgdGVzdGUiLCJlbWFpbCI6InRlc3RlQGVtYWlsLmNvbSIsImltYWdlIjoibnVsbCJ9LCJpYXQiOjE2MjAyNDQxODcsImV4cCI6MTYyMDY3NjE4N30.Roc4byj6mYakYqd9LTCozU1hd9k_Vw5IWKGL4hcCVG8';
+    return { token };
+};
 
 module.exports = {
   createUser,
