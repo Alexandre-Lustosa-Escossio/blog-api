f2d80127e9f26d78911323f2abecabdbb31f76ab
Alexandre <lutosaalexandre@gmail.com>
2022-06-10 22:01:06 -0300

    feat: category model implemented and req 07 completed

---
+++ src/database/models/category.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+  })  
+  return Category
+};
