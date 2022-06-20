cd0d4dd7dec45eccaad9df6f40b699f01547227c
Alexandre <lutosaalexandre@gmail.com>
2022-06-20 16:21:40 -0300

    feat: req 08 completed

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,6 +1,7 @@
 const { Category } = require('../database/models');
 
 const addCategory = async (body) => {
+  // Remeber to validate if category already exists in the database
   const response = await Category.create(body);
   return response;
 };
