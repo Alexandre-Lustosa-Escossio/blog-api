51fd7143fbff3868c511fbe41627db8e582368cc
Alexandre <lutosaalexandre@gmail.com>
2022-06-20 21:29:23 -0300

    feat: get userId from request token implemented

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -2,7 +2,7 @@ const postService = require('../services/postService');
 
 const addPost = async (req, res, next) => {
   try {
-    const response = await postService.addPost(req.body);
+    const response = await postService.addPost(req);
     return res.status(201).json(response);
   } catch (e) {
     next(e);
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,8 +1,15 @@
 const { BlogPost } = require('../database/models');
+const tokenHandler = require('../middlewares/tokenHandler');
+const userService = require('./userService');
 
-const addPost = async (body) => {
-  console.log(body);
-  const response = await BlogPost.create(body);
+const addPost = async ({ body, headers }) => {
+  const { authorization } = headers;
+  const { data: userEmail } = tokenHandler.decodeToken(authorization);
+  const { dataValues: { id } } = await userService.getUserByEmail(userEmail);
+  const postPayload = { ...body, id };
+  const postCategoryIds = postPayload.categoryIds;
+  delete postPayload.categoryIds;
+  const response = await BlogPost.create(postPayload);
   return response;
 };
 
